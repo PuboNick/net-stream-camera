a19fc71f089efe78b78d516c7059b8aa6f135fed
Pubo Nick <360104322@qq.com>
2025-11-17 18:01:17 +0800

    feat(channel):修改摄像头配置

--- apps/rtp-client/src/config.js
+++ apps/rtp-client/src/config.js
@@ -2,13 +2,7 @@ exports.videos = [
   {
     id: 'camera_1',
     name: '海康主摄',
-    input: 'rtsp://admin:shansu606@192.168.99.64.252:554/h264/ch1/sub/av_stream',
+    input: 'rtsp://admin:yikun606@192.168.2.252:554/h264/ch1/sub/av_stream',
     port: 61206,
-  },
-  {
-    id: 'camera_2',
-    name: '海康红外',
-    input: 'rtsp://admin:shansu606@192.168.99.64:554/h264/ch2/sub/av_stream',
-    port: 61208,
   },
 ];
